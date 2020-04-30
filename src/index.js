2d71a8ed73f01fe7c9c0f744eb016fae61dcd504
root <root@localhost.localdomain>
2020-04-30 10:18:24 +0300

    3. Reducer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 console.log('Hellow World!');
+
+const reducer = (state = 0, action) => {
+
+  switch (action.type) {
+    case 'INC': 
+      return state + 1;
+    default: 
+      return state;
+  }
+};
+
+let state = reducer(undefined, {});
+
+state = reducer(state, { type: 'INC' });
+
+console.log(state);
+
+state = reducer(state, { type: 'INC' });
+
+console.log(state);
