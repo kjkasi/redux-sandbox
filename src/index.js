060e4fdba75c6dda2724247d5e0117c0b07a4298
root <root@localhost.localdomain>
2020-04-30 22:32:35 +0300

    14. mapDispatchToProps в виде объекта

--- src/index.js
+++ src/index.js
@@ -4,25 +4,9 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import App from './components/app.js';
-
 import reducer from './reducer';
 
-
 const store = createStore(reducer);
-
-/* 
-<Counter 
-      counter={store.getState()} 
-      inc={inc}
-      dec={dec}
-      rnd={() => {
-        const value = Math.floor(Math.random() * 10);
-        rnd(value);
-      }} />
-
-      const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
-
-*/
 
 ReactDOM.render(
   <Provider store={store}>
